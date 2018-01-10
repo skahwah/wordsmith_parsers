5491287765149e176d4a7641f2d7707546761ff2
Tom Porter <porter.thomas.w@gmail.com>
2018-01-10 14:19:55 -0500

    USA geonames code used for Wordsmith 2.0.8

--- geonames-parsers/usa-geonames.rb
+++ geonames-parsers/usa-geonames.rb
@@ -2,7 +2,7 @@
 #Encoding: UTF-8
 # Written by Tom Porter
 # Twitter @porterhau5
-# 10/24/17
+# 01/10/18
 
 require 'csv'
 require 'fileutils'
@@ -21,30 +21,30 @@ all_files.each do |file|
   puts "boundary #{boundary}"
   puts "mapping #{mapping[boundary]}"
 
-  waters_array = ["Arroyo","Bay","Bend","Canal","Channel","Falls","Glacier","Gut","Harbor","Lake","Rapids","Reservoir","Sea","Spring","Stream","Swamp"]
+  waters_array = ["Arroyo","Bar","Basin","Bay","Beach","Bend","Canal","Cape","Channel","Falls","Glacier","Gut","Harbor","Island","Isthmus","Lake","Rapids","Reservoir","Sea","Spring","Stream","Swamp"]
 
-  buildings_array = ["Airport","Bridge","Building","Cemetery","Church","Crossing","Dam","Hospital","Military","Mine","Oilfield","Post Office","School","Tower","Tunnel","Well"]
+  structures_array = ["Airport","Bridge","Building","Cemetery","Church","Crossing","Dam","Hospital","Military","Mine","Oilfield","Post Office","School","Tower","Tunnel","Well"]
 
-  lands_array = ["Arch","Area","Bench","Cliff","Crater","Flat","Forest","Gap","Levee","Park","Pillar","Plain","Range","Reserve","Ridge","Slope","Summit","Valley","Woods"]
+  lands_array = ["Arch","Area","Bench","Cliff","Crater","Flat","Forest","Gap","Lava","Levee","Park","Pillar","Plain","Range","Reserve","Ridge","Slope","Summit","Trail","Valley","Woods"]
 
-  islands_array = ["Bar","Basin","Beach","Cape","Island","Isthmus"]
+  places_array = ["Census","Civil","Locale","Populated Place"]
 
   waters = []
-  buildings = []
+  structures = []
   lands = []
-  islands = []
+  places = []
 
   CSV.foreach(file, encoding: "iso-8859-1", headers: :first_row, col_sep: "|", quote_char: "\x00") do |row|
-    if waters_array.include? row[2] then waters << row[1].gsub(" (historical)","") end
-    if buildings_array.include? row[2] then buildings << row[1].gsub(" (historical)","") end
-    if lands_array.include? row[2] then lands << row[1].gsub(" (historical)","") end
-    if islands_array.include? row[2] then islands << row[1].gsub(" (historical)","") end
+    if waters_array.include? row[2] then waters << row[1].gsub(" (historical)","").gsub(" Historic District","") end
+    if structures_array.include? row[2] then structures << row[1].gsub(" (historical)","").gsub(" Historic District","") end
+    if lands_array.include? row[2] then lands << row[1].gsub(" (historical)","").gsub(" Historic District","") end
+    if places_array.include? row[2] then places << row[1].gsub(" (historical)","").gsub(" Census Designated Place","").gsub(" Historic District","") end
   end
 
   waters.sort!.uniq!
-  buildings.sort!.uniq!
+  structures.sort!.uniq!
   lands.sort!.uniq!
-  islands.sort!.uniq!
+  places.sort!.uniq!
 
   path = "#{mapping[boundary]}"
   FileUtils.mkdir_p(path)
@@ -53,16 +53,16 @@ all_files.each do |file|
     waters.each {|l| f.puts(l)}
   end
 
-  File.open("#{path}/buildings.txt","w" ) do |f|
-    buildings.each {|l| f.puts(l)}
+  File.open("#{path}/structures.txt","w" ) do |f|
+    structures.each {|l| f.puts(l)}
   end
 
   File.open("#{path}/lands.txt","w" ) do |f|
     lands.each {|l| f.puts(l)}
   end
 
-  File.open("#{path}/islands.txt","w" ) do |f|
-    islands.each {|l| f.puts(l)}
+  File.open("#{path}/places.txt","w" ) do |f|
+    places.each {|l| f.puts(l)}
   end
 
 end
